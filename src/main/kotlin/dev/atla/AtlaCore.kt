8e0e221d8fbc3e33fc38082de1d901b83c48cdda
Tc554 <tomerweidler2@gmail.com>
2024-09-21 20:12:41 +0300

    init

---
+++ src/main/kotlin/dev/atla/AtlaCore.kt
@@ -0,0 +1,14 @@
+package dev.atla
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AtlaCore : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
